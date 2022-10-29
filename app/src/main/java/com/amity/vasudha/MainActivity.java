4590142ee612c4cd217d6f4b8d6e3077e9bb9255
LAKSHYA KUMAR <smartlakshyakumar@gmail.com>
2022-10-30 00:46:53 +0530

    Workplan - Commit 1

--- app/src/main/java/com/amity/vasudha/MainActivity.java
+++ app/src/main/java/com/amity/vasudha/MainActivity.java
@@ -10,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // List Factory Locations in App
+        // Get API Quote
+        // Process AQI information
+        // Display Data onto app
+        // Send data to govt.
+
+
     }
 }
