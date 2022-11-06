4ad2d1c12602ecc9b0bca1fe7e8ee25656853f29
Abhay <abhay_nf@outlook.com>
2022-11-06 18:20:51 +0530

    Push1

--- app/src/main/java/com/amity/vasudha/MainActivity.java
+++ app/src/main/java/com/amity/vasudha/MainActivity.java
@@ -1,6 +1,7 @@
 package com.amity.vasudha;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
 
 import android.content.Intent;
 import android.os.Bundle;
@@ -8,32 +9,33 @@ import android.view.View;
 import android.widget.Button;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Button MoveToList;
 
+    private CardView card1,card2,card3;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        card1 = (CardView) findViewById(R.id.card_1);
+        card2 = (CardView) findViewById(R.id.card_2);
+        card3 = (CardView) findViewById(R.id.card_3);
 
-        MoveToList = findViewById(R.id.button1);
-        MoveToList.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent1 = new Intent(view.getContext(), MainActivity2.class);
-                startActivity(intent1);
-            }
-        });
+        card1.setOnClickListener(this);
+        card2.setOnClickListener(this);
+        card3.setOnClickListener(this);
+    }
 
-        // List Factory Locations in App
-        // Get API Quote
-        // Process AQI information
-        // Display Data onto app
-        // Send data to govt.
+    @Override
+    public void onClick(View v) {
+        Intent i ;
 
+        switch (v.getId()){
+            case R.id.card_1: i = new Intent(this,MainActivity2.class); startActivity(i); break;
+            case R.id.card_2: i = new Intent(this,MainActivity3.class); startActivity(i); break;
+            case R.id.card_3: i = new Intent(this,MainActivity4.class); startActivity(i); break;
+            default:break;
 
-
-
+        }
     }
 }
