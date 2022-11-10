793d42ccbb730366fe07b3225f615de4c2cf3897
LAKSHYA KUMAR <smartlakshyakumar@gmail.com>
2022-11-10 22:42:29 +0530

    @abhay code push kar

--- app/src/main/java/com/amity/vasudha/MainActivity3.java
+++ app/src/main/java/com/amity/vasudha/MainActivity3.java
@@ -3,12 +3,25 @@ package com.amity.vasudha;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity3 extends AppCompatActivity {
+
+    TextView abc;
+    String value = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
+
+        Bundle extras = getIntent().getExtras();
+        if (extras != null) { value = extras.getString("dataSendKey"); }
+        else { value = "nothing"; }
+
+        abc = findViewById(R.id.textViewlol);
+        abc.setText(value);
+
+
     }
 }
